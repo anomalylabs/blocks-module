02916dcfecc7ec3aef4c23c4575d5c2d5b208ad5
Ryan Thompson <service@aiwebsystems.com>
2018-10-31 00:13:49 -0500

    Border Style

---
+++ resources/lang/en/style.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'border_style' => [
+        'label'  => 'Border Style',
+        'option' => [
+            'none'   => 'None',
+            'hidden' => 'Hidden',
+            'dotted' => 'Dotted',
+            'dashed' => 'Dashed',
+            'solid'  => 'Solid',
+            'double' => 'Double',
+            'groove' => 'Groove',
+            'ridge'  => 'Ridge',
+            'inset'  => 'Inset',
+            'outset' => 'Outset',
+        ],
+    ],
+];
