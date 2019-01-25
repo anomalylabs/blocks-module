e7877a11721ce9234f05924ec297d73d95bd2274
Martin Modler <modler@webformatik.com>
2019-01-25 08:08:30 +0100

    Added empty BlockObserver

---
+++ src/Block/BlockObserver.php
@@ -0,0 +1,15 @@
+<?php namespace Anomaly\BlocksModule\Block;
+
+use Anomaly\Streams\Platform\Entry\EntryObserver;
+
+/**
+ * Class BlockObserver
+ *
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
+ */
+class BlockObserver extends EntryObserver
+{
+
+}
