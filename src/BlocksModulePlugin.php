eb6f0c094c063ec7f1013698ad7d9688b06e5bf3
Ryan Thompson <service@aiwebsystems.com>
2019-05-28 14:19:31 -0500

    Docs and patch function for blocks

--- src/BlocksModulePlugin.php
+++ src/BlocksModulePlugin.php
@@ -31,7 +31,7 @@ class BlocksModulePlugin extends Plugin
                         return null;
                     }
 
-                    return view('anomaly.module.blocks::areas/blocks', compact('area'));
+                    return $area->getBlocks();
                 }, ['is_safe' => ['html']]
             ),
         ];
